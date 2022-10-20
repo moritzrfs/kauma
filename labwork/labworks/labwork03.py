3116880e6c629fba02b0c1b88efce5222fe20803
Moritz <moritz.rfs@gmail.com>
2022-10-20 21:27:04 +0200

    fix cleanup unused vars and useless comments

--- labwork/labworks/labwork03.py
+++ labwork/labworks/labwork03.py
@@ -19,31 +19,18 @@ def query_oracle_padding(_session : requests.Session, keyname: str, iv: str, cip
     return result.text
 
 def handle_pkcs7_padding(assignment):
-    iv = assignment["iv"]
-    # iv base64 decode
-    iv = base64.b64decode(iv)
-    # iv as bytearray
+    iv = base64.b64decode(assignment["iv"])
     iv = bytearray(iv)
-
     ciphertext = base64.b64decode(assignment["ciphertext"])
-
-    # get bytesize of the ciphertext
-    bytesize = len(ciphertext)
-
-    # create empty block of 16 bytes
-    test_iv = bytearray(16)
-
-    # create bytearray xor_result
-    xor_result = bytearray(16)
-    # loop 16 times
+    test_iv = bytearray(16) # create empty test_iv to test against the oracle
+    xor_result = bytearray(16) # create emptyy bytearray xor_result
     counter = 0
-    for i in range(16, 0, -1): # 15
+    for i in range(16, 0, -1): # loop through the 16 byte block starting from the last byte
         counter += 1            
         test_iv_pad = search_correct_padding(test_iv, ciphertext, assignment["keyname"], i-1)
         xor_result = prepare_iv(test_iv_pad, i-1, xor_result, counter)
     
     plain = decrypt_cbc(xor_result, iv)
-    # plain base64 encode and decode utf8
     plain_b64 = base64.b64encode(plain).decode("utf8")
     return {"plaintext": plain_b64 }
     
@@ -82,27 +69,20 @@ def search_correct_padding(test_iv, ciphertext, key, round):
 def prepare_iv(test_iv: bytearray, round: int, xor_result: bytearray, counter: int) -> bytearray:
     xor_result[round] = test_iv[round] ^ counter
     print("prepare_iv:", xor_result)
-    # xor_result print as hex
     print(xor_result.hex())
     for i in range (0, counter):
         test_iv[15-i] = xor_result[15-i] ^ counter+1
     print("test_iv:", test_iv)
-    # test_iv print as hex
     print(test_iv.hex())
     return xor_result
 
 def decrypt_cbc(xor_result: bytearray, iv: bytearray):
     plaintext = b''
     for i in range(0, 16):
-        plaintext += bytes([xor_result[i] ^ iv[i]])
-    
-    # get last byte of plaintext
-    last_byte = plaintext[-1]
-    # check if last x bytes of plaintext are equal to last_byte
-    if plaintext[-last_byte:] == bytes([last_byte] * last_byte):
-        # remove last x bytes from plaintext
-        plaintext = plaintext[:-last_byte]
+        plaintext += bytes([xor_result[i] ^ iv[i]])    
+    last_byte = plaintext[-1] # get last byte of plaintext
+
+    if plaintext[-last_byte:] == bytes([last_byte] * last_byte): # check if last x bytes of plaintext are equal to last_byte
+         plaintext = plaintext[:-last_byte] # remove last x bytes from plaintext
 
     return plaintext
-
-handle_pkcs7_padding(test_assignment)
