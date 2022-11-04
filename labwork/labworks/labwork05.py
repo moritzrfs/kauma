492b5232eebf70a9c185597f1f4904358f0611de
moritzrfs <moritz.rfs@gmail.com>
2022-11-04 10:48:36 +0100

    add finalization for 05

--- labwork/labworks/labwork05.py
+++ labwork/labworks/labwork05.py
@@ -1,6 +1,7 @@
 import base64
 import json
 import sys
+
 import requests
 
 api_endpoint = sys.argv[1]
@@ -36,19 +37,16 @@ def handle_rc4_fms(assignment, tcid):
         test_result = handle_test_result(key, tcid)
         round += 1
         search_arr = occurence_tree(search_arr, round)
-        # print("Key:", key)
     key = base64.b64encode(key).decode('utf-8')
-    print("Key as base64:", key)
+    print("Key:", key, "found with", round, "rounds")
     return {"key" : key}
 
-def filter_ivs(ivs: list[bytes], byte_index: int, key: bytearray) -> list:
+def filter_ivs(ivs: list, byte_index: int, key: bytearray) -> list:
     results = []
-    # convert ivs to bytearray
     ivs = [bytearray(iv) for iv in ivs]
     for i in range(len(ivs)):
         if ivs[i][0] == byte_index + 3 and ivs[i][1] == 255:
-            # ksa mit 3 schritten auf iv
-            if  byte_index == 0:
+            if  byte_index == 0: # ksa 3 steps on iv
                 iv_with_key = ivs[i][0:3]
             else:
                 iv_with_key = bytearray(ivs[i][0:3] + key[0:byte_index])
@@ -58,7 +56,7 @@ def filter_ivs(ivs: list[bytes], byte_index: int, key: bytearray) -> list:
             results.append(result_key)
     return results
 
-def get_occurence(results: list, search_arr: list, i: int) -> dict:
+def get_occurence(results: list, search_arr: list, i: int) -> dict: # search_arr defines position and max-nth occurence of a candidate
     occurences = {}
     for item in results:
         if (item in occurences):
@@ -67,11 +65,11 @@ def get_occurence(results: list, search_arr: list, i: int) -> dict:
             occurences[item] = 1
     if search_arr[0] == i:
         result = sorted(occurences.items(), key=lambda x: x[1], reverse=True)[search_arr[1]][0]
-    else:
+    else: 
         result = sorted(occurences.items(), key=lambda x: x[1], reverse=True)[0][0]    
     return result
 
-def invert_sbox(sbox: list[int]) -> list[int]:
+def invert_sbox(sbox: list) -> list:
     inverse = [0] * len(sbox)
     for i in range(len(sbox)):
         inverse[sbox[i]] = i
@@ -87,35 +85,10 @@ def handle_test_result(key, tcid):
     else:
         return False
 
-def occurence_tree(arr, round):
+def occurence_tree(arr, round): # helper function to define position and max-nth occurence of a candidate
     if (round -1) % 16 == 0:
         arr[1] += 1
         arr[0] = 0
     else:
         arr[0] += 1
-    # print("Occurence tree:", arr, "Round:", round)
     return arr
-
-def bonus():
-    ksa = []
-    sbox = [i for i in range(256)]
-
-    pseudoj = 0
-    for i in range(256):
-        new = 256 - pseudoj 
-        ksa.append(new)
-        pseudoj = (pseudoj + sbox[i] + ksa[i]) % 256
-        
-    j = 0
-    for i in range(256):
-        print("j: ",j)
-        print("sbox[i]: ",sbox[i])
-        print("ksa[i]: ",ksa[i])
-        j = (j + sbox[i] + ksa[i]) % 256
-        print("Ergebnis: ",j,i)
-        print()
-        sbox[i], sbox[j] = sbox[j], sbox[i]
-    print(ksa)
-    print(sbox)
-
-bonus()
